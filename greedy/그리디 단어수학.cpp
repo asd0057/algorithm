c41b68d7334f3b5e931a67a9a266ea9fb1c14d60
asd0057 <tnsdnjs0057@gmail.com>
2021-01-16 14:44:50 +0900

    Create 그리디 단어수학.cpp
    https://www.acmicpc.net/problem/1339

---
+++ greedy/그리디 단어수학.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool a(int a, int b) {
+	if (a > b) return true;
+	else return false;
+}
+
+int main()
+{
+	int n;
+	int result[26] = {0};
+	string alph;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> alph;
+		for (int j = 0; j < alph.size(); j++) {
+			result[alph[j] - 'A'] += pow(10, alph.size() - 1 - j);
+		}
+	}
+
+	sort(result, result + 26, a);
+
+	int last = 0;
+	for (int i = 0; i < 10; i++) {
+		last += result[i] * (9 - i);
+	}
+
+	cout << last;
+}
+
+
