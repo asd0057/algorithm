8ec332e37d5113df263582b20822cebf63494b19
asd0057 <tnsdnjs0057@gmail.com>
2021-01-17 00:25:15 +0900

    Create 그리디 1080번 행렬.cpp
    //1080번 행렬 https://www.acmicpc.net/problem/1080
    
    //입력 n, m의 행렬에서 3*3의 행렬을 0->1 1->0으로 뒤집어
    //처음 입력된 A행렬에서 B행렬로 바꿀수있는가.
    
    //힌트 - 뒤집기를 수행할 때 뒤집은 수에서 확실히 A행렬과 B행렬이 일치한다고 보장할 수 있는것은
    //뒤집기를 시작하는 점이며 n-2, m-2까지는 3*3행렬 바꾸기의 범위를 뛰어넘기 때문에
    //뒤집기 시작점이 될수없어 A와 B가 같다고 보장될  수 없고, 이부분을 검사한다.

---
+++ greedy/그리디 1080번 행렬.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M;
+char a[50][50], b[50][50];
+int result;
+
+void change(int x, int y)
+{
+    for (int i = x; i < x + 3; i++)
+    {
+        for (int j = y; j < y + 3; j++)
+        {
+            if (a[i][j] == '1') a[i][j] = '0';
+            else a[i][j] = '1';
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> a[i][j];
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> b[i][j];
+
+    for (int i = 0; i < N - 2; i++)
+    {
+        for (int j = 0; j < M - 2; j++)
+        {
+            if (a[i][j] != b[i][j])
+            {
+                change(i, j);
+                result++;
+            }
+        }
+    }
+
+    bool isDifferent = false;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (a[i][j] != b[i][j])
+            {
+                isDifferent = true;
+            }
+        }
+    }
+
+    if (isDifferent) cout << -1;
+    else cout << result;
+
+    return 0;
+}
