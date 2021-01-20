4de57362530b536d2b2487a257eb74a63ad97d29
asd0057 <tnsdnjs0057@gmail.com>
2021-01-21 01:17:41 +0900

    Create 병든나이트.cpp
    조건문을 정확하게 처리하여
    실패를 방지해야 하는 문제.
    입력값이 2000000000일 경우에는
    절대 탐색이 아닌 그리디를 통해 최적화 정답을 찾아야 함.

---
+++ greedy/병든나이트.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+
+int main() {
+	long long int N, M;
+	long long int max = 1;
+	scanf_s("%lld %lld", &N, &M);
+	//N이 1일떄 대비 안되있음.
+	if (N == 1) {
+		max = 1;
+	} 
+	else if (N >= 3) {
+		if (M >= 6) {
+			max = M - 2;
+		}
+		else {
+			if (M >= 4) {
+				max = 4;
+			}
+			else {
+				max = M;
+			}
+		}
+	}
+	else {
+		if (M >= 7) {
+			max = 4;
+		}
+		else {
+			if (M % 2 == 0) {
+				max = M / 2;
+			}
+			else {
+				max = (M / 2) + (M % 2);
+			}
+		}
+	}
+
+	//if (N == 1) {
+	//	max = 1;
+	//}
+	//else if (N == 2)
+	//{
+	//	max = (M + 1) / 2 > 4 ? 4 : (M + 1) / 2;
+	//}
+	//else {
+	//	if (M > 6) {
+	//		max = M - 2;
+	//	}
+	//	else {
+	//		max = M > 4 ? 4 : M;
+	//	}
+	//}
+	printf("%lld", max);
+
+}
