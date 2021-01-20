7acd1f5764a3975e306212bd309a05f1a438b527
asd0057 <tnsdnjs0057@gmail.com>
2021-01-21 01:18:25 +0900

    Create 1449 수리공.cpp
    sort를 통한 입력값의 오름차순 (처음에는 입력값 그대로 사용하여 정렬되지 않아서 1%에러 발생)
    입력값이 무조건 오름차순 되어있다는 생각을 해선 안됨.

---
+++ greedy/1449 수리공.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+bool comp2(const int& a, const int& b) {
+	return a < b;
+}
+int main() {
+	int N, L;
+	int mat[1000];
+	scanf_s("%d %d", &N, &L);
+	int v;
+	float max = -0.5;
+	int count = 0;
+
+	float a = 0.5f;
+	int b = 0;
+
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &mat[i]);
+	}
+
+	std::sort(mat, mat + N, comp2);
+
+	for (int i = 0; i < N; i++) {
+		if (max < mat[i]) {
+			max = mat[i] + L - 0.5;
+			count++;
+		}
+	}
+
+	printf("%d", count);
+}
