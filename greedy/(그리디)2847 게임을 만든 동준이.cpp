a4c158c442afb4982e6c2d5dfa1166920085f9dd
asd0057 <tnsdnjs0057@gmail.com>
2021-01-23 00:31:43 +0900

    Create (그리디)2847 게임을 만든 동준이.cpp
    0부터 N-1검사를 진행하면 정해지지 않은 뒷쪽 숫자에 의해 연산이 불가능하다.
    N-1숫자는 최고 레벨의 점수인것을 가정하여 MAX값으로 설정하고 -1씩 처리를 진행한다.

---
+++ greedy/(그리디)2847 게임을 만든 동준이.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+
+int main() {
+	int N;
+	int arr[100];
+	scanf_s("%d", &N);
+	
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	int max = arr[N - 1];
+	int count = 0;
+	for (int i = N-2; i >= 0; i--) {
+		if (max <= arr[i]) {
+			count += arr[i] - (max - 1);
+			max = max - 1;
+		}
+		else {
+			max = arr[i];
+		}
+	}
+
+
+	printf("%d", count);
+}
